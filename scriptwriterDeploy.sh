e912f852b4373f62a5f02e800299d34d41c8e009
Andy Boyle <andy@sudostream.io>
2017-01-20 14:20:21 +0000

    adding script for deploying scriptwriter

---
+++ scriptwriterDeploy.sh
@@ -0,0 +1,14 @@
+gcloud container clusters get-credentials api-event-horizon-cluster
+scriptwriter-bump.sh
+
+git add dev/kubernetes-scriptwriter-deployment.yaml
+git commit -m "bump"
+git push -u origin master
+
+kubectl delete secret scriptwriter-tls
+kubectl create secret generic scriptwriter-tls --from-file $HOME/.ssh/certs
+kubectl delete configmap nginx-dev-proxf-conf
+kubectl create configmap nginx-dev-proxf-conf --from-file ./dev/nginx.conf
+kubectl apply -f ./dev/kubernetes-scriptwriter-service.yaml --record
+kubectl apply -f ./dev/kubernetes-scriptwriter-deployment.yaml --record
+
