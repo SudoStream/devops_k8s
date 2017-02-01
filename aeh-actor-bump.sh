a952782aa8bb501ee249522bd70ef0e3fc8e964f
Andy Boyle <andy@sudostream.io>
2017-02-01 16:21:23 +0000

    bump

--- aeh-actor-bump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-oldNum=`cat dev/kubernetes-aeh-actor-deployment.yaml | grep bump | cut -d "-" -f2`
-newNum=`expr $oldNum + 1`
-sed -i "s/bump-$oldNum/bump-$newNum/g" dev/kubernetes-aeh-actor-deployment.yaml
