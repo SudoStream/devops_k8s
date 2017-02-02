ef5e6b830b67003c9bb38b5a5fdc1c413d856083
Andy Boyle <andy@sudostream.io>
2017-02-02 22:06:56 +0000

    tidy the scripts. hostNetork=true commented out

--- aehActorDeploy.sh
+++ aehActorDeploy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+shouldskip=$1
 
-gcloud container clusters get-credentials api-event-horizon-cluster
-./aeh-actor-bump.sh
+if [[ ${shouldskip} != "local" ]]; then
+    gcloud container clusters get-credentials api-event-horizon-cluster
 
-git add dev/kubernetes-aeh-actor-deployment.yaml
-git commit -m "bump"
-git push -u origin master
+    oldNum=`cat dev/kubernetes-aeh-actor-deployment.yaml | grep bump | cut -d "-" -f2`
+    newNum=`expr $oldNum + 1`
+    sed -i "s/bump-$oldNum/bump-$newNum/g" dev/kubernetes-aeh-actor-deployment.yaml
+
+    git add dev/kubernetes-aeh-actor-deployment.yaml
+    git commit -m "bump"
+    git push -u origin master
+fi
 
 kubectl delete secret scriptwriter-tls
 kubectl create secret generic scriptwriter-tls --from-file $HOME/.ssh/certs
--- scriptwriterDeploy.sh
+++ scriptwriterDeploy.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+shouldskip=$1
 
-gcloud container clusters get-credentials api-event-horizon-cluster
-./scriptwriter-bump.sh
+if [[ ${shouldskip} != "local" ]]; then
+    gcloud container clusters get-credentials api-event-horizon-cluster
 
-git add dev/kubernetes-scriptwriter-deployment.yaml
-git commit -m "bump"
-git push -u origin master
+    #bump
+    oldNum=`cat dev/kubernetes-scriptwriter-deployment.yaml | grep bump | cut -d "-" -f2`
+    newNum=`expr $oldNum + 1`
+    sed -i "s/bump-$oldNum/bump-$newNum/g" dev/kubernetes-scriptwriter-deployment.yaml
+
+    # push bump to git
+    git add dev/kubernetes-scriptwriter-deployment.yaml
+    git commit -m "bump"
+    git push -u origin master
+fi
 
 kubectl delete secret scriptwriter-tls
 kubectl create secret generic scriptwriter-tls --from-file $HOME/.ssh/certs
@@ -13,4 +21,3 @@ kubectl delete configmap nginx-scriptwriter-dev-proxf-conf
 kubectl create configmap nginx-scriptwriter-dev-proxf-conf --from-file ./dev/nginx-scriptwriter.conf
 kubectl apply -f ./dev/kubernetes-scriptwriter-service.yaml --record
 kubectl apply -f ./dev/kubernetes-scriptwriter-deployment.yaml --record
-
