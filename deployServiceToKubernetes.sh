d090dfe71c3c72ab304a8e7426c5a6a7bd57d9cd
Andy Boyle <andy@sudostream.io>
2017-09-08 14:03:30 +0100

    create cacerts secret

--- deployServiceToKubernetes.sh
+++ deployServiceToKubernetes.sh
@@ -96,5 +96,7 @@ kubectl delete --ignore-not-found secret ${serviceToDeploy}-tls
 kubectl create secret generic ${serviceToDeploy}-tls --from-file $HOME/.ssh/certs
 kubectl delete --ignore-not-found configmap nginx-${serviceToDeploy}-dev-proxf-conf
 kubectl create configmap nginx-${serviceToDeploy}-dev-proxf-conf --from-file ./dev/nginx-${serviceToDeploy}.conf
+kubectl delete secret mongodbkeystore
+kubectl create secret generic mongodbkeystore --from-file=cacerts=/etc/ssl/cacerts
 kubectl apply -f ./dev/kubernetes-${serviceToDeploy}-service.yaml --record
 kubectl apply -f ./dev/kubernetes-${serviceToDeploy}-deployment.yaml --record
